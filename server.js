6eb61c4bedb42c4cffbf16321966cf9a8016bd58
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2016-12-18 20:19:31 +0500

    Changing repo for heroku live server

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ var express = require('express')
 
 // tell express where to look for static assets
 //app.use(express.static(__dirname + '/public'));
+
+app.use(function (req, res, next) {
+	if (req.header['x-forwarded-proto'] === 'http') {
+		next();
+	} else {
+		res.redirect('http://' + req.hostname + req.url);
+	}
+});
+
 app.use(express.static('public'));
 
 // start our server
