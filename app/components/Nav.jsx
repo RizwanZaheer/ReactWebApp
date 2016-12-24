8e64f79c21a3267e3035727071a7fb559085bdcc
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2016-12-25 00:01:46 +0500

    style nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,14 +1,31 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = () => {
-    return(
-        <div>
-            <h1>Nav bar</h1>
-            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
-            <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</Link>
-        </div>
-    );
-}
-
+var Nav = React.createClass({
+    render: function () {
+        return(
+            <div className="top-bar">
+              <div className="top-bar-left">
+                <ul className="dropdown menu" data-dropdown-menu>
+                  <li className="menu-text">React App</li>
+                  <li>
+                    <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
+                  </li>
+                  <li>
+                      <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="top-bar-right">
+                <form onSubmit={this.onSearch}>
+                    <ul className="menu">
+                      <li><input type="search" placeholder="Search"/></li>
+                      <li><button type="button" className="button">Search</button></li>
+                    </ul>
+                </form>
+              </div>
+            </div>
+        );
+    }
+});
 module.exports = Nav;
